8a4c2f007b8b464422bcebe14e8a1f91cf584f8b
Mahrukh-aan <mahrukhaan005@gmail.com>
2023-05-20 23:30:27 +0500

    Integrated My Recipes

--- screens/MyRecipes.js
+++ screens/MyRecipes.js
@@ -1,5 +1,13 @@
 import React, {useEffect, useState, useContext} from 'react';
-import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+  Image,
+} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import Myrec1 from '../assets/images/myrec1.svg';
 import Myrec2 from '../assets/images/myrec2.svg';
@@ -13,47 +21,62 @@ export default function MyRecipes({route, navigation}) {
   const {user} = useContext(AuthContext);
   const email = user?.data?.user?.email;
   const userId = user?.data?.user?._id;
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const viewMyRecipes = async res => {
+    setLoading(true);
+    var data = JSON.stringify({
+      email: email,
+    });
+    try {
+      const response = await axios({
+        method: 'post',
+        url: endpoint + '/recipes/userRecipes',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: data,
+      });
+
+      console.log(JSON.stringify(response.data));
+      setRecipes(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  useEffect(() => {
+    viewMyRecipes();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <TouchableOpacity style={styles.box3}>
-          <Myrec1
-            width={(70 / dim.w) * dim.Width}
-            height={(50 / dim.w) * dim.Width}
-            style={{marginRight: (20 / dim.w) * dim.Width}}
-          />
-          <View style={{width: (220 / dim.w) * dim.Width}}>
-            <Text style={styles.caltxt}>250 Kcal</Text>
-            <Text style={styles.name}>Chopped Spring Ramen</Text>
-            <Text style={[styles.desc, {marginTop: 0}]}>
-              Scallions & tomatoes
-            </Text>
-          </View>
-          <Hearticon
-            width={(20 / dim.w) * dim.Width}
-            height={(20 / dim.w) * dim.Width}
-            style={styles.hicon}
-          />
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.box3}>
-          <Myrec2
-            width={(70 / dim.w) * dim.Width}
-            height={(60 / dim.w) * dim.Width}
-            style={{marginRight: (20 / dim.w) * dim.Width}}
-          />
-          <View style={{width: (220 / dim.w) * dim.Width}}>
-            <Text style={styles.caltxt}>450 Kcal</Text>
-            <Text style={styles.name}>Chicken Tandoori</Text>
-            <Text style={styles.desc}>Chicken & Salad</Text>
-          </View>
-          <Hearticon
-            width={(20 / dim.w) * dim.Width}
-            height={(20 / dim.w) * dim.Width}
-            style={styles.hicon}
-          />
-        </TouchableOpacity>
-      </ScrollView>
+      {loading ? (
+        <ActivityIndicator></ActivityIndicator>
+      ) : (
+        <FlatList
+          data={recipes}
+          renderItem={({index, item}) => (
+            <View key={index}>
+              <TouchableOpacity
+                style={[styles.box3, {backgroundColor: '#EBF2FF'}]}
+                onPress={() => {
+                  navigation.navigate('ViewRecipe', {title: item.Title});
+                }}>
+                <Image
+                  source={{
+                    uri: endpoint + '/' + item.Image.filename,
+                  }}
+                  style={styles.thumbnail}
+                />
+                <Text style={[styles.name, {width: 250}]}>{item.Title}</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        />
+      )}
     </View>
   );
 }
@@ -66,36 +89,26 @@ const styles = StyleSheet.create({
   },
 
   box3: {
-    height: (120 / dim.h) * dim.Height,
+    height: (80 / dim.h) * dim.Height,
     width: (350 / dim.w) * dim.Width,
     borderRadius: 12,
-    marginVertical: (8 / dim.h) * dim.Height,
+    margin: (10 / dim.w) * dim.Width,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#EFF7EE',
   },
 
   name: {
-    fontSize: 18,
+    fontSize: 16,
     color: 'black',
-    fontFamily: 'Inter-Medium',
-  },
-  desc: {
-    fontSize: 14,
-    color: '#7B6F72',
-    fontFamily: 'Inter-Light',
-  },
-
-  caltxt: {
-    fontSize: 14,
-    color: '#91C788',
     fontFamily: 'Inter-SemiBold',
+    marginTop: (5 / dim.h) * dim.Height,
   },
-
-  hicon: {
-    alignSelf: 'flex-start',
-    marginTop: (10 / dim.h) * dim.Height,
-    marginRight: (5 / dim.w) * dim.Width,
+  thumbnail: {
+    width: (50 / dim.w) * dim.Width,
+    height: (50 / dim.w) * dim.Width,
+    marginRight: 10,
+    borderRadius: 25,
+    backgroundColor: 'red',
   },
 });
